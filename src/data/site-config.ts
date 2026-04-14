054f9bf10f95300a509c4ff9a42b99869ae9f60e
ALDO YAIR <arias.aldo333@gmail.com>
2026-04-13 21:54:59 -0600

    change of data

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -8,8 +8,8 @@ const siteConfig: SiteConfig = {
         src: avatar,
         alt: 'Ethan Donovan'
     },
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
+    title: 'Aldo Arias',
+    subtitle: 'QA Engineer',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
         src: '/dante-preview.jpg',
